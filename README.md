a206972b426944eb302730c5c7608196a01506c0
Adrian Odbierzychleb <adrian.odbierzychleb@gmail.com>
2019-11-01 09:38:45 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Practice of Java Connection with Database
---
+++ src/main/java/com/spartaglobal/javadatabaseconnections/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.javadatabaseconnections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
